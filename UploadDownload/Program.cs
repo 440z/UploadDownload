485c30349e5db430b8f07031f17d883dd163d789
440z <hisdudeness777@web.de>
2021-06-24 17:14:20 +0200

    ILoggerFactory loggerFactory; hat auf die schnelle nicht geklappt. Ich wollte mich nicht laenger damit aufhalten

--- UploadDownload/Program.cs
+++ UploadDownload/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +13,15 @@ namespace UploadDownload
 {
     public class Program
     {
+        ILoggerFactory loggerFactory;
+
+        public Program()
+        {
+            // install-package Microsoft.Extensions.Logging.Console
+            //loggerFactory = new LoggerFactory().AddConsole((categoryName, logLevel) => (logLevel == LogLevel.Information) && (categoryName == DbLoggerCategory.Database.Command.Name));
+            loggerFactory = new LoggerFactory().
+        }
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
